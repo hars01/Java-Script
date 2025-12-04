d11aba9aa5596b24421b1b47db4d10046691f8e5
Harsh Mishra <harsh7565023881@gmail.com>
2025-12-04 15:39:16 +0530

    datatype-summary

--- Rivison/01_basics/04_comparison.js
+++ Rivison/01_basics/04_comparison.js
@@ -5,8 +5,8 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
@@ -18,4 +18,5 @@ console.log(undefined < 0);
 
 // === 
 
+console.log("2" == 2);
 console.log("2" === 2);
---
+++ Rivison/01_basics/datatype-summary.js
@@ -0,0 +1,55 @@
+// Primitive
+// 7 types: String, Number, Boolean, Null, undefined, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=true
+const outsideTemp=null
+let userEmail=undefined // or also use like that let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id==anotherId);
+console.log(id===anotherId);
+
+const bigNumber=1234567890123456789012345678901234567890n
+console.log(bigNumber);
+
+
+
+// Reference Datatype
+// 3 types: Object, Array, Functions
+
+const hero=["shaktiman","spiderman","batman", "superman", "ironman", "thor", "captain america", "captain India"];
+console.log(hero);
+console.log(typeof hero);
+
+
+
+let myObj={
+    name:"hitesh",
+    age:18,
+    isLoggedIn:true,
+}
+console.log(myObj);
+console.log(typeof myObj);
+
+
+
+
+const greet=function () {
+    console.log("hi harsh");
+}
+greet();
+console.log(typeof greet);
+
+
+function greet2() { 
+    console.log("hello harsh");
+}
+
+greet2();
+console.log(typeof greet2);
+
